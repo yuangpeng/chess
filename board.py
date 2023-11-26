51e4cbbc38b240800eeebba2bc9c1a67d29c2100
Yuang Peng <yuangpeng.cn@gmail.com>
2023-11-26 15:21:29 +0800

    Complete basic functions

--- board.py
+++ board.py
@@ -7,6 +7,9 @@ from collections import deque
 from abc import ABC, abstractmethod
 from enum import Enum
 from loguru import logger
+import pickle
+import copy
+from collections import deque
 
 
 class Color(Enum):
@@ -15,36 +18,76 @@ class Color(Enum):
     WHITE = "WHITE"
 
 
-class BaseBoard(ABC):
+class BaseBoardGame(ABC):
     def __init__(self, size: int):
+        self.name = ""
         self.size = size
         self.board = [[Color.EMPTY for _ in range(size)] for _ in range(size)]
         self.round = 0
+        self.history: list[Memento] = []  # Use a list of Mementos for the history
+
+    def cur_player(self) -> Color:
+        return Color.BLACK if self.round % 2 == 0 else Color.WHITE
 
     @abstractmethod
-    def move(self, coord: tuple(int, int) | None = None):
+    def move(self, coord: tuple[int, int] | None = None):
+        raise NotImplementedError
+
+    @abstractmethod
+    def regret(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def restart(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_memento(self) -> Memento:
+        raise NotImplementedError
+
+    @abstractmethod
+    def restore_from_memento(self, memento: Memento):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_to_file(self, file_path: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def load_from_file(self, file_path: str):
         raise NotImplementedError
 
 
-class GoGame(BaseBoard):
+class Memento:
+    def __init__(self, state: dict):
+        self.__state = state
+
+    def get_saved_state(self):
+        return self.__state
+
+
+class GoGame(BaseBoardGame):
     # Rule 1: Go is played on a 19x19 square grid of points, by two players called Black and White.
     def __init__(self, size: int):
         # Rule 2: Each point on the grid may be colored black, white or empty.
         super().__init__(size)
+        self.name = "Go Game"
         self.komi = 6.5
-        self.ko_point: tuple(int, int) | None = None
+        self.ko_point: tuple[int, int] | None = None
         self.last_move_captured = None
         self.abstention = 0
 
-    def move(self, coord: tuple(int, int) | None = None):
+    def move(self, coord: tuple[int, int] | None = None):
         if coord is not None:
             # Rule 6: A turn is either a pass or a move that doesn’t repeat an earlier grid coloring (superko).
             if self.ko_point == coord:
                 logger.info("Cannot recapture ko immediately.")
                 return
 
+            self.history.append(self.create_memento())
+
             # Rule 5: Starting with an empty grid, the players alternate turns, starting with Black.
-            current_color = Color.BLACK if self.round % 2 == 0 else Color.WHITE
+            current_color = self.cur_player()
             opposite_color = Color.WHITE if current_color == Color.BLACK else Color.BLACK
             x, y = coord
             # Rule 7: A move consists of coloring an empty point one’s own color; then clearing the opponent color, and then clearing one’s own color.
@@ -59,7 +102,8 @@ class GoGame(BaseBoard):
                 self.ko_point = None
 
             self.clear(coord)
-        else:
+        else:  # pass
+            self.history.append(self.create_memento())
             self.abstention += 1
             # Rule 8: The game ends after two consecutive passes.
             if self.abstention == 2:
@@ -70,9 +114,57 @@ class GoGame(BaseBoard):
 
         self.round += 1
 
-    def neighbors(self, coord: tuple(int, int)) -> list(tuple(int, int)):
+    def regret(self):
+        if len(self.history) > 0:
+            self.restore_from_memento(self.history.pop())
+            logger.info("Move undone.")
+        else:
+            self.restart()
+
+    def restart(self):
+        self.__init__(self.size)
+
+    def create_memento(self) -> Memento:
+        # Save the current state in a memento
+        state = {
+            "name": self.name,
+            "size": self.size,
+            "board": copy.deepcopy(self.board),
+            "round": self.round,
+            # "history": copy.deepcopy(self.history),
+            "komi": self.komi,
+            "ko_point": self.ko_point,
+            "last_move_captured": self.last_move_captured,
+            "abstention": self.abstention,
+        }
+        return Memento(state)
+
+    def restore_from_memento(self, memento: Memento):
+        # Restore the state from the memento
+        state = memento.get_saved_state()
+        self.name = state["name"]
+        self.size = state["size"]
+        self.board = state["board"]
+        self.round = state["round"]
+        # self.history = state["history"]
+        self.komi = state["komi"]
+        self.ko_point = state["ko_point"]
+        self.last_move_captured = state["last_move_captured"]
+        self.abstention = state["abstention"]
+
+    def save_to_file(self, file_path: str):
+        with open(file_path, "wb") as file:
+            pickle.dump(self.create_memento(), file)
+            logger.info("Game saved to file.")
+
+    def load_from_file(self, file_path: str):
+        with open(file_path, "rb") as file:
+            self.restore_from_memento(pickle.load(file))
+            logger.info("Game loaded from file.")
+
+    def neighbors(self, coord: tuple[int, int]) -> list[tuple[int, int]]:
         x, y = coord
-        return [(x, ny) for ny in (y - 1, y + 1) if 1 <= ny <= self.size] + [(nx, y) for nx in (x - 1, x + 1) if 1 <= nx <= self.size]
+        return [(x, ny) for ny in (y - 1, y + 1) if 0 <= ny < self.size] + [(nx, y) for nx in (x - 1, x + 1) if 0 <= nx < self.size]
 
     # Rule 4: Clearing a color is the process of emptying all points of that color that don’t reach empty.
     def clear(self, points: tuple[int, int] | list[tuple[int, int]]) -> set[tuple[int, int]]:
@@ -88,7 +180,7 @@ class GoGame(BaseBoard):
         return captured
 
     # Rule 3: A point P, not colored C, is said to reach C if there is a path of (vertically or horizontally) adjacent points of P’s color from P to a point of color C.
-    def string(self, coord: tuple[int, int]) -> list(tuple[int, int]):
+    def string(self, coord: tuple[int, int]) -> list[tuple[int, int]]:
         visited = set()
         visited.add(coord)
 
@@ -102,7 +194,7 @@ class GoGame(BaseBoard):
 
         return visited
 
-    def liberties(self, group: list(tuple[int, int])) -> bool:
+    def liberties(self, group: list[tuple[int, int]]) -> bool:
         empty = [nbr for p in group for nbr in self.neighbors(p) if self.board[nbr[0]][nbr[1]] == Color.EMPTY]
         return len(empty) > 0
 
@@ -130,10 +222,10 @@ class GoGame(BaseBoard):
 
         return black_territory, white_territory
 
-    def remove_dead_stones(self) -> tuple(int, int):
+    def remove_dead_stones(self) -> tuple[int, int]:
         return 0, 0
 
-    def flood_fill(self, start: tuple(int, int)):
+    def flood_fill(self, start: tuple[int, int]):
         queue = deque([start])
         territory = set([start])
         borders = set()
@@ -161,71 +253,3 @@ class GoGame(BaseBoard):
         white_score = len(white_territory) + black_captures + self.komi
 
         return black_score, white_score
-
-
-import pygame
-import sys
-
-# Constants
-BOARD_SIZE = 19
-GRID_SIZE = 40
-WINDOW_SIZE = GRID_SIZE * (BOARD_SIZE + 1)
-STONE_RADIUS = GRID_SIZE // 2 - 2
-
-# Colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BACKGROUND = (175, 135, 0)
-go_game = GoGame(BOARD_SIZE)
-
-# Initialize pygame
-pygame.init()
-
-# Create the screen
-screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-pygame.display.set_caption("Go Game")
-
-# Initialize the board
-# board = [[None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-# current_color = BLACK
-
-
-def draw_board():
-    screen.fill(BACKGROUND)
-    for row in range(BOARD_SIZE):
-        for col in range(BOARD_SIZE):
-            pygame.draw.rect(screen, BLACK, (GRID_SIZE + col * GRID_SIZE - 1, GRID_SIZE + row * GRID_SIZE - 1, 2, 2))
-            if go_game.board[row][col] in (Color.BLACK, Color.WHITE):
-                if go_game.board[row][col] == Color.BLACK:
-                    draw_color = BLACK
-                else:
-                    draw_color = WHITE
-                pygame.draw.circle(screen, draw_color, (GRID_SIZE + col * GRID_SIZE, GRID_SIZE + row * GRID_SIZE), STONE_RADIUS)
-
-
-def handle_mouse_click(pos):
-    # global current_color
-    x, y = pos
-    row = round((y - GRID_SIZE) / GRID_SIZE)
-    col = round((x - GRID_SIZE) / GRID_SIZE)
-
-    if 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE and go_game.board[row][col] == Color.EMPTY:
-        go_game.move((row, col))
-        draw_board()  # Redraw the board to show the updated state
-
-
-# Game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            handle_mouse_click(pygame.mouse.get_pos())
-            draw_board()  # Redraw the board after every click
-
-    pygame.display.flip()
-
-
-pygame.quit()
-sys.exit()
--- gui.py
+++ gui.py
@@ -1,135 +1,150 @@
 from __future__ import annotations
-import tkinter as tk
-from collections import deque
 
-
-class Color:
-    EMPTY = 0
-    BLACK = 1
-    WHITE = 2
-
-
-# 你的 GoGame 类代码 ...
-class GoGame:
-    def __init__(self, size: int):
-        self.size = size
-        self.board = [[Color.EMPTY for _ in range(size)] for _ in range(size)]
-        self.__turns = 0
-
-    def move(self, coord: tuple[int, int] | None = None):
-        if coord is not None:
-            x, y = coord
-            if self.turns % 2 == 0:
-                self.board[x][y] = Color.BLACK
-            else:
-                self.board[x][y] = Color.WHITE
-            opponent = [nbr for nbr in self.neighbors(coord) if self.board[nbr[0]][nbr[1]] != self.board[x][y] and self.board[nbr[0]][nbr[1]] != Color.EMPTY]
-            self.clear(opponent)
-            self.clear(coord)
-
-        self.__turns += 1
-
-    def neighbors(self, coord: tuple[int, int]) -> list(tuple[int, int]):
-        x, y = coord
-        return [(x, ny) for ny in (y - 1, y + 1) if 1 <= ny <= self.size] + [(nx, y) for nx in (x - 1, x + 1) if 1 <= nx <= self.size]
-
-    def clear(self, points: tuple[int, int] | list[tuple[int, int]]):
-        if isinstance(points, tuple):
-            points = [points]
-        captured = set()
-        for pt in points:
-            str_points = self.string(pt)
-            if not self.liberties(str_points):
-                captured.update(str_points)
-        for cap in captured:
-            self.board[cap[0]][cap[1]] = Color.EMPTY
-
-    def string(self, coord: tuple[int, int]) -> list(tuple[int, int]):
-        visited = set()
-        visited.add(coord)
-
-        queue = deque([coord])
-
-        while queue:
-            for nbr in self.neighbors(queue.popleft()):
-                if self.board[nbr[0]][nbr[1]] == self.board[coord[0]][coord[1]] and nbr not in visited:
-                    visited.add(nbr)
-                    queue.append(nbr)
-
-        return visited
-
-    def liberties(self, group: list(tuple[int, int])) -> bool:
-        empty = [nbr for p in group for nbr in self.neighbors(p) if self.board[nbr[0]][nbr[1]] == Color.EMPTY]
-        return len(empty) > 0
-
-    @property
-    def turns(self):
-        return self.__turns
-
-
-import pygame
 import sys
 
-# Constants
-BOARD_SIZE = 19
-GRID_SIZE = 40
-WINDOW_SIZE = GRID_SIZE * (BOARD_SIZE + 1)
-STONE_RADIUS = GRID_SIZE // 2 - 2
+import pygame
 
-# Colors
+from board import Color, BaseBoardGame, GoGame
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BACKGROUND = (175, 135, 0)
-go_game = GoGame(BOARD_SIZE)
-
-# Initialize pygame
-pygame.init()
-
-# Create the screen
-screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
-pygame.display.set_caption("Go Game")
-
-# Initialize the board
-# board = [[None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-# current_color = BLACK
 
 
-def draw_board():
-    screen.fill(BACKGROUND)
-    for row in range(BOARD_SIZE):
-        for col in range(BOARD_SIZE):
-            pygame.draw.rect(screen, BLACK, (GRID_SIZE + col * GRID_SIZE - 1, GRID_SIZE + row * GRID_SIZE - 1, 2, 2))
-            if go_game.board[row][col] in (Color.BLACK, Color.WHITE):
-                if go_game.board[row][col] == Color.BLACK:
-                    draw_color = BLACK
-                else:
-                    draw_color = WHITE
-                pygame.draw.circle(screen, draw_color, (GRID_SIZE + col * GRID_SIZE, GRID_SIZE + row * GRID_SIZE), STONE_RADIUS)
+class Button:
+    def __init__(self, x, y, width, height, text, callback):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.callback = callback
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, WHITE, self.rect)
+        font = pygame.font.SysFont(None, 24)
+        text_surface = font.render(self.text, True, BLACK)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.callback()
 
 
-def handle_mouse_click(pos):
-    # global current_color
-    x, y = pos
-    row = round((y - GRID_SIZE) / GRID_SIZE)
-    col = round((x - GRID_SIZE) / GRID_SIZE)
+class BoardGameGUI:
+    def __init__(self, game: BaseBoardGame, grid_size: int = 40):
+        self.game = game
 
-    if 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE and go_game.board[row][col] == Color.EMPTY:
-        go_game.move((row, col))
-        draw_board()  # Redraw the board to show the updated state
+        # Initialize pygame, and create the screen
+        pygame.init()
+        self.grid_size = grid_size
+        self.windows_size = self.grid_size * (self.game.size + 1)
+        self.screen = pygame.display.set_mode((self.windows_size, self.windows_size))
+        pygame.display.set_caption(f"{self.game.name}")
+        self.stone_radius = self.grid_size // 2 - 2
+
+    def draw_current_player(self):
+        font = pygame.font.SysFont(None, 36)
+        if self.game.cur_player() == Color.BLACK:
+            text = font.render("Current Player: Black", True, BLACK)
+        else:
+            text = font.render("Current Player: White", True, WHITE)
+        self.screen.blit(text, (10, 10))
+
+    def draw_board(self):
+        self.screen.fill(BACKGROUND)
+        for row in range(self.game.size):
+            for col in range(self.game.size):
+                pygame.draw.rect(
+                    self.screen,
+                    BLACK,
+                    (self.grid_size + col * self.grid_size - 1, self.grid_size + row * self.grid_size - 1, 2, 2),
+                )
+                if self.game.board[row][col] in (Color.BLACK, Color.WHITE):
+                    if self.game.board[row][col] == Color.BLACK:
+                        draw_color = BLACK
+                    else:
+                        draw_color = WHITE
+                    pygame.draw.circle(
+                        self.screen,
+                        draw_color,
+                        (self.grid_size + col * self.grid_size, self.grid_size + row * self.grid_size),
+                        self.stone_radius,
+                    )
+
+    def create_buttons(self):
+        self.buttons = [
+            Button(10, self.windows_size - 70, 100, 50, "Undo", self.undo_move),
+            Button(120, self.windows_size - 70, 100, 50, "Restart", self.restart_game),
+            Button(230, self.windows_size - 70, 100, 50, "Pass", self.pass_turn),
+            Button(340, self.windows_size - 70, 100, 50, "Save", lambda: self.save_game_state("game_state.txt")),
+            Button(450, self.windows_size - 70, 100, 50, "Load", lambda: self.load_game_state("game_state.txt")),
+        ]
+
+    def draw_buttons(self):
+        for button in self.buttons:
+            button.draw(self.screen)
+
+    def undo_move(self):
+        self.game.regret()
+
+    def restart_game(self):
+        self.game.restart()
+
+    def pass_turn(self):
+        self.game.move(None)
+
+    def save_game_state(self, filename):
+        self.game.save_to_file(filename)
+
+    def load_game_state(self, filename):
+        self.game.load_from_file(filename)
+
+    def handle_mouse_click(self, pos: tuple(float, float)):
+        x, y = pos
+        row = round((y - self.grid_size) / self.grid_size)
+        col = round((x - self.grid_size) / self.grid_size)
+
+        if 0 <= row < self.game.size and 0 <= col < self.game.size and self.game.board[row][col] == Color.EMPTY:
+            self.game.move((row, col))
+
+    def start_game(self):
+        self.create_buttons()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        for button in self.buttons:
+                            button.handle_event(event)
+                        pos = pygame.mouse.get_pos()
+                        if pos[1] < self.windows_size - 80:
+                            self.handle_mouse_click(pos)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_u:
+                        self.undo_move()
+                    elif event.key == pygame.K_r:
+                        self.restart_game()
+                    elif event.key == pygame.K_p:
+                        self.pass_turn()
+                    elif event.key == pygame.K_s:
+                        self.save_game_state("game_state.txt")
+                    elif event.key == pygame.K_l:
+                        self.load_game_state("game_state.txt")
+                    elif event.key == pygame.K_q:
+                        running = False
+                self.draw_board()
+                self.draw_current_player()
+                self.draw_buttons()
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
 
 
-# Game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            handle_mouse_click(pygame.mouse.get_pos())
-            draw_board()  # Redraw the board after every click
-
-    pygame.display.flip()
-
-
-pygame.quit()
-sys.exit()
+if __name__ == "__main__":
+    # 初始化游戏和GUI
+    go_game = GoGame(19)
+    gui = BoardGameGUI(go_game)
+    gui.start_game()
